efc4dcba9897125541408c6ee32b85feb05700b8
Jason Krause <jason.krause@darkwolf.co>
2021-04-02 13:22:31 -0600

    updating variables.tf formatting

--- variables.tf
+++ variables.tf
@@ -5,13 +5,13 @@ variable "kube_conf_file" {
 }
 
 variable "big_bang_manifest_file" {
-    type = string
-    default = "k8s/start.yaml"
+  type    = string
+  default = "k8s/start.yaml"
 }
 
 variable "registry_credentials" {
   type = list(object({
-    registry   = string
+    registry = string
     username = string
     password = string
   }))
