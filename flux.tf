4fcb80bc318b1f9c3e459daa22af4c2dca155c00
Rob Kernick <rjkernick@gmail.com>
2021-04-21 11:32:27 -0400

    Add ability to reduce flux usage requests

--- flux.tf
+++ flux.tf
@@ -2,9 +2,21 @@ data "kubectl_file_documents" "flux" {
   content = file("${path.module}/k8s/flux.yaml")
 }
 
+data "kubectl_file_documents" "flux_light" {
+  content = file("${path.module}/k8s/flux_light.yaml")
+}
+
 resource "kubectl_manifest" "flux_deployment" {
-  count     = length(data.kubectl_file_documents.flux.documents)
+  count     = var.reduce_flux_resources == true ? 0 : length(data.kubectl_file_documents.flux.documents)
   yaml_body = element(data.kubectl_file_documents.flux.documents, count.index)
+  depends_on = [
+    kubernetes_secret.private_registry
+  ]
+}
+
+resource "kubectl_manifest" "flux_deployment_light" {
+  count     = var.reduce_flux_resources == true ? length(data.kubectl_file_documents.flux_light.documents) : 0
+  yaml_body = element(data.kubectl_file_documents.flux_light.documents, count.index)
   depends_on = [
     kubernetes_secret.private_registry
   ]
