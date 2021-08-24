95e642d84efa2e33eb6d259c38d13e4adafee7af
Andrew Roth <andrew.roth@leapfrog.ai>
2021-08-24 16:30:31 -0700

    Add description to output external_load_balancer

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,4 @@
 output "external_load_balancer" {
+  description = "JSON object containing information about the external load balancer, such as its IP address"
   value = data.external.Wait_for_load_balancer.result
 }
