25de86fe05e07f60a3da356989fee13bff7a30d6
brian.rexrode <brian.rexrode@defenseunicorns.com>
2021-09-15 10:59:00 -0600

    fix flux

--- flux.tf
+++ flux.tf
@@ -1,10 +1,10 @@
 data "kubectl_file_documents" "flux" {
-#  content = var.custom_flux_file == "" ? file("${path.module}/k8s/flux.yaml") : file(var.custom_flux_file)
-  content = file(var.custom_flux_file)
+  content = var.custom_flux_file == "" ? file("${path.module}/k8s/flux.yaml") : file(var.custom_flux_file)
 }
 
 resource "kubectl_manifest" "flux_deployment" {
-  yaml_body = element(data.kubectl_file_documents.flux.documents,0)
+  count     = length(data.kubectl_file_documents.flux.documents)
+  yaml_body = element(data.kubectl_file_documents.flux.documents, count.index)
   depends_on = [
     kubernetes_secret.private_registry
   ]
