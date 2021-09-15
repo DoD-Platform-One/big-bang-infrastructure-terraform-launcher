634b2fd33ca4589710981bd0918af7fc2dec0132
brian.rexrode <brian.rexrode@defenseunicorns.com>
2021-09-15 10:46:48 -0600

    testing

--- flux.tf
+++ flux.tf
@@ -1,5 +1,6 @@
 data "kubectl_file_documents" "flux" {
-  content = var.custom_flux_file == "" ? file("${path.module}/k8s/flux.yaml") : file(var.custom_flux_file)
+#  content = var.custom_flux_file == "" ? file("${path.module}/k8s/flux.yaml") : file(var.custom_flux_file)
+  content = file(var.custom_flux_file)
 }
 
 resource "kubectl_manifest" "flux_deployment" {
