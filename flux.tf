f73960f7351fe2366d39f575f146fffd9a84b6f8
Darcy Cleaver <darcy@leapfrog.ai>
2021-10-05 10:07:59 -0600

    put configmap back debug

--- flux.tf
+++ flux.tf
@@ -6,21 +6,20 @@ resource "kubectl_manifest" "flux_deployment" {
   count     = length(data.kubectl_file_documents.flux.documents)
   yaml_body = element(data.kubectl_file_documents.flux.documents, count.index)
   depends_on = [
-    kubernetes_secret.private_registry,
-    kubernetes_config_map.controller-config
+    kubernetes_secret.private_registry
   ]
 }
 
-resource "kubernetes_config_map" "controller-config" {
-  count = var.custom_flux_file == "" ? 0 : 1
-  metadata {
-    name = "controller-config"
-    namespace = "flux-system"
-  }
+# resource "kubernetes_config_map" "controller-config" {
+#   # count = var.custom_flux_file == "" ? 0 : 1
+#   metadata {
+#     name = "controller-config"
+#     namespace = "flux-system"
+#   }
 
-  data = {
-    ".bashrc" = "${file("${path.module}/configs/.bashrc")}"
-    ".bash_profile" = "${file("${path.module}/configs/.bash_profile")}"
-    "config" = "${file("${path.module}/configs/config")}"
-  }
-}
+#   data = {
+#     ".bashrc" = "${file("${path.module}/configs/.bashrc")}"
+#     ".bash_profile" = "${file("${path.module}/configs/.bash_profile")}"
+#     "config" = "${file("${path.module}/configs/config")}"
+#   }
+# }
