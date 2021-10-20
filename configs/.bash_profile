4c63cf84c44be843b5032f9309aa67b81345fcba
Andrew Roth <andrew.roth@leapfrog.ai>
2021-10-20 16:59:04 +0000

    Flux override input

---
+++ configs/.bash_profile
@@ -0,0 +1,9 @@
+# required for immutable filesytem requirements within flux kustomize-controller pod to allow for cloning private git repos
+# .bash_profile
+
+# Get the aliases and functions
+if [ -f ~/.bashrc ]; then
+	. ~/.bashrc
+fi
+
+# User specific environment and startup programs
--- variables.tf
+++ variables.tf
@@ -1,14 +1,21 @@
 variable "kube_conf_file" {
   description = "Path to the KUBECONFIG file to use to connect to the cluster. If the file passed has multiple contexts in it the correct context is expected to already be set in `contexts.current-context`."
-  type    = string
-  default = "~/.kube/config"
+  type        = string
+  default     = "~/.kube/config"
 }
 
 variable "big_bang_manifest_file" {
   description = "Path to the root k8s yaml manifest. Typically contains a Namespace, GitRepository, and Kustomization. See https://repo1.dso.mil/platform-one/quick-start/big-bang/-/blob/master/bigbang/start.yaml for example"
-  type    = string
-  default = "k8s/start.yaml"
+  type        = string
+  default     = "k8s/start.yaml"
 }
+
+variable "custom_flux_file" {
+  description = "Path to the optional custom flux k8s yaml manifest"
+  type        = string
+  default     = ""
+}
+
 
 variable "registry_credentials" {
   description = "System-wide registry credentials to be applied so Kubernetes can pull container images. Creds for registry1.dso.mil are required, and you can optionally provide any other creds for other private registries as well"
@@ -19,8 +26,13 @@ variable "registry_credentials" {
   }))
 }
 
-variable "reduce_flux_resources" {
-  description = "DEPRECATED - Used to tweak resource settings to fit on a smaller machine, but the new Flux deployment already uses the smaller values. `flux.yaml` and `flux_light.yaml` are now identical files."
-  type    = bool
-  default = false
+variable "custom_credentials" {
+  description = "Any custom credentials needed for custom BigBang implementation"
+  type = list(object({
+    namespace = string
+    name      = string
+    username  = string
+    password  = string
+  }))
+  default = []
 }
